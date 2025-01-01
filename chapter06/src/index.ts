82a172f6c3d649c306aeade0a36ae38241b71085
ieiriyuki <ieiriyuki@exam.com>
2025-01-01 19:45:34 +0900

    feat: conditional types

--- chapter06/src/index.ts
+++ chapter06/src/index.ts
@@ -130,3 +130,14 @@ const numbers2: FruitArrays = {
     orange: ['orange', 'orange', 'orange', 'orange', 'orange'],
     strawberry: ['strawberry'],
 }
+
+// conditional types
+type RestArgs<M> = M extends "string" ? [string, string] : [number, number, number];
+function func<M extends "string" | "number">(
+    mode: M,
+    ...args: RestArgs<M>
+) {
+    console.log(mode, ...args);
+}
+func("string", "uhyo", "hyo")
+func("number", 1, 2, 3)
